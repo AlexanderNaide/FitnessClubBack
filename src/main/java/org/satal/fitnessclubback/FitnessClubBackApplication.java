30ab47b1a7e2d7aa283ff41eb2695fd5f580af43
Alexandr <kb_299@bk.ru>
2023-06-30 15:10:05 +0300

    Initial commit

---
+++ src/main/java/org/satal/fitnessclubback/FitnessClubBackApplication.java
@@ -0,0 +1,13 @@
+package org.satal.fitnessclubback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessClubBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessClubBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/satal/fitnessclubback/FitnessClubBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.satal.fitnessclubback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessClubBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
